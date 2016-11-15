8fdfcd8aab3fdd3b43deec56d3c455116cca06ab
Maksym Pinchuk <Maksym_Pinchuk@epam.com>
2016-11-15 13:23:52 +0200

    first commit

---
+++ src/epam/com/Main.java
@@ -0,0 +1,10 @@
+package epam.com;
+
+/**
+ * Created by Maksym_Pinchuk on 11/15/2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
